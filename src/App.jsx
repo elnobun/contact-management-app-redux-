5ac1a6f18b452ee14be055426e790ec6129f0b10
Ellis Enobun <elnobun@gmail.com>
2018-07-27 17:08:46 +0100

    Redux setup & contact reducer

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,29 @@ import AddContact from './components/contacts/AddContact';
 import EditContact from './components/contacts/EditContact';
 import NotFound from './components/static/NotFound';
 import About from './components/static/About';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header />
-          <div className="container" style={{ paddingTop: '6em' }}>
-            <Switch>
-              <Route exact path="/" component={Contacts} />
-              <Route exact path="/contact/add" component={AddContact} />
-              <Route exact path="/contact/edit/:id" component={EditContact} />
-              <Route exact path="/about" component={About} />
-              <Route component={NotFound} />
-            </Switch>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <div className="container" style={{ paddingTop: '6em' }}>
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
+                <Route exact path="/about" component={About} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }

9f2eb013c9d811f7deae1cae2cbfde4181b5677f
Ellis Enobun <elnobun@gmail.com>
2018-07-27 18:35:18 +0100

    jsonplaceholder GET request with contact action.

--- src/redux/actions/contactAction.js
+++ src/redux/actions/contactAction.js
@@ -1,9 +1,14 @@
 import { GET_CONTACTS, ADD_CONTACTS, DELETE_CONTACTS } from './types';
+import axios from 'axios';
 
 // Action: get contacts
-export const getContacts = () => ({
-  type: GET_CONTACTS
-});
+export const getContacts = () => async dispatch => {
+  const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+  dispatch({
+    type: GET_CONTACTS,
+    payload: res.data
+  });
+};
 
 // Action: add contacts by {contact}
 export const addContact = contact => ({
--- src/redux/reducers/contactReducer.js
+++ src/redux/reducers/contactReducer.js
@@ -1,26 +1,7 @@
 import { GET_CONTACTS, ADD_CONTACTS, DELETE_CONTACTS } from '../actions/types';
 
 const initialState = {
-  contacts: [
-    {
-      id: 1,
-      name: 'James Doe',
-      email: 'james@gmail.com',
-      phone: 111 - 111 - 111
-    },
-    {
-      id: 2,
-      name: 'Willaim Harts',
-      email: 'willy@gmail.com',
-      phone: 222 - 222 - 222
-    },
-    {
-      id: 3,
-      name: 'Harry Johnson',
-      email: 'harry@gmail.com',
-      phone: 333 - 333 - 333
-    }
-  ]
+  contacts: []
 };
 
 export default (state = initialState, action) => {
@@ -28,7 +9,8 @@ export default (state = initialState, action) => {
     // get contacts from original state
     case GET_CONTACTS:
       return {
-        ...state
+        ...state,
+        contacts: action.payload
       };
     // add contacts through spread operator
     case ADD_CONTACTS:
